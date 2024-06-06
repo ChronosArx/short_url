a8821881ff7cda5be6e2c6268ae97c95cd6487df
David Arias <chronos.software.dev@gmail.com>
2024-06-06 01:13:56 -0600

    set alembic for migrations and create tables free url and users

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from core import shorten_router
+from database.data_base_conf import Base, engine
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(shorten_router.router)
