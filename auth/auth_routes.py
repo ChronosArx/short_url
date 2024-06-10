3e871e7afb0a8ad4e5a18f0b146ec5223065cf9b
David Arias <chronos.software.dev@gmail.com>
2024-06-10 02:42:58 -0600

    creaicon de sevicios y rutas para authentificación

--- auth/auth_routes.py
+++ auth/auth_routes.py
@@ -1,13 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from typing import Annotated
+import auth_services as services
+import auth.auth_schema as auth_schema
+from share_dependencies.database_dependency import get_db
 
 router = APIRouter()
 
 
 @router.post("/signup")
-async def signup():
-    pass
+async def signup(
+    user: auth_schema.UserSignUpSchema, db: Annotated[any, Depends(get_db)]
+):
+    return services.signup(user=user, db=db)
 
 
 @router.post("/login")
-async def login():
-    pass
+async def login(user: auth_schema.UserLogInSchema, db: Annotated[any, Depends(get_db)]):
+    return services.login(user=user, db=db)
--- auth/auth_services.py
+++ auth/auth_services.py
@@ -1,7 +1,35 @@
 from sqlalchemy.orm import Session
-import auth_dtos
+from fastapi import HTTPException, status
+import auth.auth_schema as auth_schema
 from ..database import models
+from ..utils.generate_hash import getPasswordHash, checkPassword
 
 
-def signup(db:Session, user:auth_dtos.UserSignUpDTO):
-   pass
+def signup(db: Session, user: auth_schema.UserSignUpSchema):
+    hash_password = getPasswordHash(user.password)
+    db_user = models.User(
+        user_name=user.user_name,
+        email=user.email,
+        password=hash_password,
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def login(db: Session, user: auth_schema.UserLogInSchema):
+    db_user = (
+        db.query(models.User).filter(models.User.user_name == user.user_name).first()
+    )
+    if not db_user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found!",
+        )
+    if not checkPassword(user.password, db_user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="The password is incorrect!",
+        )
+    return db_user
