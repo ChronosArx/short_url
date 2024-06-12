47bb3f6d7102e6187c02b435623feecb18c4f783
David Arias <chronos.software.dev@gmail.com>
2024-06-12 00:29:17 -0600

    create the function verify token

--- utils/jwt_encoders.py
+++ utils/jwt_encoders.py
@@ -1,9 +1,10 @@
-from fastapi import HTTPException
+from fastapi import HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer
 import jwt
-from jwt.exceptions import ExpiredSignatureError
-from dotenv import load_dotenv
+from jwt.exceptions import ExpiredSignatureError, PyJWTError
 from datetime import datetime, timezone, timedelta
+from dotenv import load_dotenv
+from typing import Annotated
 import os
 
 load_dotenv()
@@ -12,7 +13,8 @@ token_algorithm = os.environ.get("TOKEN_ALG")
 secret = os.environ.get("SECRET")
 
 
-schema_oauth = OAuth2PasswordBearer(tokenUrl='login')
+schema_oauth = OAuth2PasswordBearer(tokenUrl="login")
+
 
 def generate_access_token(user_id: int):
     expire = datetime.now(tz=timezone.utc) + timedelta(minutes=15)
@@ -21,21 +23,22 @@ def generate_access_token(user_id: int):
     return token
 
 
-def generate_refresh_token(token:str):
-   exception = HTTPException(
+def generate_refresh_token():
+    pass
+
+
+def verify_token(token: Annotated[str, Depends(schema_oauth)]):
+    exception = HTTPException(
         status_code=401,
         detail="Error de Credenciales",
         headers={"WWW-Authenticate": "Bearer"},
     )
+
     try:
         payload = jwt.decode(token, secret, algorithms=token_algorithm)
         if not payload:
             raise exception
     except ExpiredSignatureError:
         raise exception
-    except JWTError:
+    except PyJWTError:
         raise exception
-
-
-def verify_token():
-    pass
