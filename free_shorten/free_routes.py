ed3ba60955884ace0c15a0ec67d7bcbdfa75dc30
David Arias <chronos.software.dev@gmail.com>
2024-06-18 23:15:49 -0600

    quitamos el redirect del free url

--- free_shorten/free_routes.py
+++ free_shorten/free_routes.py
@@ -3,17 +3,9 @@ from ..database.database_dependency import get_db
 from typing import Annotated
 from . import free_services as services
 
-router = APIRouter(
-    prefix='/free',
-    tags=['Free']
-)
+router = APIRouter(prefix="/free", tags=["Free"])
 
 
 @router.get("/create_short_url_free")
 async def create_free_short_url(original_url: str, db: Annotated[any, Depends(get_db)]):
     return services.create_short_url(original_url=original_url, db=db)
-
-
-@router.get("/{code}")
-async def redirect_original(code: str, db: Annotated[any, Depends(get_db)]):
-    return services.redirect_url(code=code, db=db)
