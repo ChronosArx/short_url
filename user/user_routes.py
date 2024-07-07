09f674abf87d3f2637738902e579036cbbf665be
David Arias <chronos.software.dev@gmail.com>
2024-07-07 03:02:02 -0600

    estrutura para endpoints de la consulta de los codigos del usuario

---
+++ user/user_routes.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/user")
+
+
+@router.get("/all_codes")
+async def get_all_codes():
+    pass
+
+
+@router.get("/code/{code_id}")
+async def get_code():
+    pass
+
+
+@router.delete("/code/{code_id}")
+async def delate_code():
+    pass
---
+++ user/user_services.py
@@ -0,0 +1,10 @@
+def get_all_codes():
+    pass
+
+
+def get_code():
+    pass
+
+
+def delate_code():
+    pass
