72c37e7c445a748690262b87f1b850536547573d
David Arias <chronos.software.dev@gmail.com>
2024-07-11 08:00:31 -0600

    correciones al servicio shorten by user

--- shorten/shorten_services.py
+++ shorten/shorten_services.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from fastapi import HTTPException, status
 from ..database import models
 from ..utils.generate_codes import generate_short_code
 from .shorten_schemas import UrlShortenSchema, CreateShortenUrlUserSchema
@@ -17,12 +18,17 @@ def create_short_url(original_url: str, db: Session) -> UrlShortenSchema:
 def create_short_url_by_user(
     data: CreateShortenUrlUserSchema, user: str, db: Session
 ) -> UrlShortenSchema:
+    user_db = db.query(models.User).filter(models.User.user_name == user).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found."
+        )
     code = generate_short_code()
     new_short_url_user = models.Code(
-        original_ur=data.original_url,
+        original_url=data.original_url,
         title=data.title,
         code=code,
-        user=user,
+        user=user_db,
     )
     db.add(new_short_url_user)
     db.commit()
