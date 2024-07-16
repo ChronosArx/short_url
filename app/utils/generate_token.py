ccf31b052b97e945ab0c6a059a95c4a622ba924e
David Arias <chronos.software.dev@gmail.com>
2024-07-16 01:09:21 -0600

    separation of token generation functionality

---
+++ app/utils/generate_token.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timedelta, timezone
+import dotenv
+import jwt
+import os
+
+dotenv.load_dotenv()
+
+TOKEN_ALGORITHM = os.environ.get("TOKEN_ALG")
+SECRET = os.environ.get("SECRET")
+
+
+def generate_access_token(user_id: int, user_name: str):
+    expire = datetime.now(tz=timezone.utc) + timedelta(minutes=15)
+    new_payload = {"sub": str(user_id), "name": user_name, "exp ": expire.timestamp()}
+    token = jwt.encode(payload=new_payload, key=SECRET, algorithm=TOKEN_ALGORITHM)
+    return token
