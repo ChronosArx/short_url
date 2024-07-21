a42c83acb25d49c3df9d1a8b32d5f06412962be6
David Arias <chronos.software.dev@gmail.com>
2024-07-21 03:19:01 -0600

    tiempo de expiracion de tokens desde variables de entorno

--- app/utils/generate_token.py
+++ app/utils/generate_token.py
@@ -7,13 +7,17 @@ dotenv.load_dotenv()
 
 TOKEN_ALGORITHM = os.environ.get("TOKEN_ALG")
 SECRET = os.environ.get("SECRET")
+EXPIRE_ACCESS = float(os.environ.get("EXPIRE_ACCESS"))
+EXPIRE_REFRESH = float(os.environ.get("EXPIRE_REFRESH"))
 
 
 def generate_token(user_id: int, user_name: str, refresh: bool) -> str:
+    if not EXPIRE_ACCESS | EXPIRE_REFRESH | SECRET | TOKEN_ALGORITHM:
+        raise Exception("Environment variables are missing")
     if refresh:
-        expire = datetime.now(tz=timezone.utc) + timedelta(days=7)
+        expire = datetime.now(tz=timezone.utc) + timedelta(days=EXPIRE_REFRESH)
     else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=10)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=EXPIRE_REFRESH)
     new_payload = {
         "sub": str(user_id),
         "name": user_name,
