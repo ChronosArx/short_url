3d73f55671390e65a72889e5f5bbbfea69ecbe8d
David Arias <chronos.software.dev@gmail.com>
2024-07-22 22:07:30 -0600

    refactor code in generate token

--- app/utils/generate_token.py
+++ app/utils/generate_token.py
@@ -8,13 +8,21 @@ dotenv.load_dotenv()
 TOKEN_ALGORITHM = os.environ.get("TOKEN_ALG")
 SECRET = os.environ.get("SECRET")
 SECRET_REFRESH = os.environ.get("SECRET_REFRESH")
-EXPIRE_ACCESS = float(os.environ.get("EXPIRE_ACCESS"))
-EXPIRE_REFRESH = float(os.environ.get("EXPIRE_REFRESH"))
+EXPIRE_ACCESS = os.environ.get("EXPIRE_ACCESS")
+EXPIRE_REFRESH = os.environ.get("EXPIRE_REFRESH")
+
+EXPIRE_ACCESS = float(EXPIRE_ACCESS)
+EXPIRE_REFRESH = float(EXPIRE_REFRESH)
+
+if not (
+    EXPIRE_ACCESS and EXPIRE_REFRESH and SECRET and TOKEN_ALGORITHM and SECRET_REFRESH
+):
+    raise Exception("Environment variables are missing")
 
 
-def generate_token(user_id: int | None, user_name: str | None, refresh: bool) -> str:
-    if not EXPIRE_ACCESS | EXPIRE_REFRESH | SECRET | TOKEN_ALGORITHM | SECRET_REFRESH:
-        raise Exception("Environment variables are missing")
+def generate_token(
+    user_id: int = None, user_name: str = None, refresh: bool = False
+) -> str:
     if refresh:
         expire = datetime.now(tz=timezone.utc) + timedelta(days=EXPIRE_REFRESH)
         new_payload = {"exp": expire}
