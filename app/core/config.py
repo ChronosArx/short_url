5978310336f49a834a1b03138b650667ab2adfba
David Arias <chronos.software.dev@gmail.com>
2024-09-11 00:05:58 -0600

    cambio en el archivo de configuracion para el manejo de las variables de entorno

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,29 @@
 from sqlmodel import SQLModel, create_engine
+from dotenv import load_dotenv
 import os
-
 
 class Settings:
 
     def __init__(self) -> None:
+        load_dotenv()
         self.DATABASE_URL = os.getenv("DATABASE_URL")
-        print(self.DATABASE_URL)
+        self.JWT_ALGORITHM = os.environ.get("JWT_ALGORITHM")
+        self.SECRET_KEY = os.environ.get("SECRET_KEY")
+        self.EXPIRE_ACCESS = float(os.environ.get("EXPIRE_ACCESS"))
+        self.EXPIRE_REFRESH = float(os.environ.get("EXPIRE_REFRESH"))
         self.engine = create_engine(self.DATABASE_URL, echo=True)
+        self.verify_env_variables()
+
+    
+    
+    def verify_env_variables(self):
+        missing_vars = []
+        for var_name in ['EXPIRE_ACCESS', 'EXPIRE_REFRESH', 'SECRET_KEY', 'JWT_ALGORITHM', 'DATABASE_URL']:
+            if getattr(self, var_name) is None:
+                missing_vars.append(var_name)
+
+        if missing_vars:
+            raise ValueError(f"Missing environment variables: {', '.join(missing_vars)}")
 
     def get_db_metadata(self):
         return SQLModel.metadata
--- app/utils/generate_token.py
+++ app/utils/generate_token.py
@@ -1,41 +1,24 @@
 from datetime import datetime, timedelta, timezone
-import dotenv
+from ..core.config import settings
 import jwt
-import os
-
-dotenv.load_dotenv()
-
-TOKEN_ALGORITHM = os.environ.get("TOKEN_ALG")
-SECRET_KEY = os.environ.get("SECRET_KEY")
-EXPIRE_ACCESS = os.environ.get("EXPIRE_ACCESS")
-EXPIRE_REFRESH = os.environ.get("EXPIRE_REFRESH")
-
-EXPIRE_ACCESS = float(EXPIRE_ACCESS)
-EXPIRE_REFRESH = float(EXPIRE_REFRESH)
-
-
-def verify_env_variables():
-    if not (EXPIRE_ACCESS and EXPIRE_REFRESH and SECRET_KEY and TOKEN_ALGORITHM):
-        raise Exception("Environment variables are missing")
 
 
 def generate_token(
     user_id: int = None, user_name: str = None, refresh: bool = False
 ) -> str:
-    verify_env_variables()
     if refresh:
-        expire = datetime.now(tz=timezone.utc) + timedelta(days=EXPIRE_REFRESH)
+        expire = datetime.now(tz=timezone.utc) + timedelta(days=settings.EXPIRE_REFRESH)
         new_payload = {"exp": expire}
         token = jwt.encode(
-            payload=new_payload, key=SECRET_KEY, algorithm=TOKEN_ALGORITHM
+            payload=new_payload, key=settings.SECRET_KEY, algorithm=settings.JWT_ALGORITHM
         )
         return token
     else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=EXPIRE_ACCESS)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=settings.EXPIRE_ACCESS)
     new_payload = {
         "sub": str(user_id),
         "name": user_name,
         "exp": expire,
     }
-    token = jwt.encode(payload=new_payload, key=SECRET_KEY, algorithm=TOKEN_ALGORITHM)
+    token = jwt.encode(payload=new_payload, key=settings.SECRET_KEY, algorithm=settings.JWT_ALGORITHM)
     return token
